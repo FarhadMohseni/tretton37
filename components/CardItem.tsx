abcb272051d56dd33391c3de45de672541155bde
Farhad Mohseni <realfarhadmohseni@gmail.com>
2021-12-10 01:13:42 +0100

    Add fontawesome

--- components/CardItem.tsx
+++ components/CardItem.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import styles from "../styles/CardItem.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons/faTwitterSquare";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons/faGithubSquare";
+
 function CardItem() {
   return (
     <div className={styles.item}>
+      <style>{dom.css()}</style>
       <div className={styles.card}>
         <img src="mozart.jpg" className="card_image" />
         <div className={styles.card_overlay}>
-          <i className="fab fa-twitter-square"></i>
-          <i className="fab fa-github-square"></i>
-          <i className="fab fa-linkedin"></i>
+          <FontAwesomeIcon
+            icon={faTwitterSquare}
+            className={styles.card_overlay_icon}
+          />
+          <FontAwesomeIcon
+            icon={faGithubSquare}
+            className={styles.card_overlay_icon}
+          />
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={styles.card_overlay_icon}
+          />
         </div>
       </div>
       <div className={styles.item_text_container}>
